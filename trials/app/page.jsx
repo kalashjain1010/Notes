eeacda432bf6e17d1d02693b56dabd48b78537c8
kalash jain <kalashjain54@gmail.com>
2023-08-16 23:41:26 +0530

    updates

--- trials/app/page.jsx
+++ trials/app/page.jsx
@@ -6,10 +6,7 @@ const page = () => {
     // const data = 2121
     const [num, setNum] = useState("")
     const [num1, setNum1] = useState([])
-    const gg = async () => {
-        const  datagg  = await axios.get("https://jsonplaceholder.typicode.com/users")
-        // setNum1({datagg})
-    }
+  
   return (
     <div className='p-5'>
       <input type='number' className='border-2 rounded-md px-4 py-2' placeholder='enteryour num ' onChange={(e)=>{
