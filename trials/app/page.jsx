05ad9b26caa0d0b1aa92139721a34790446c4656
kalash jain <kalashjain54@gmail.com>
2023-08-18 23:38:43 +0530

    updates

--- trials/app/page.jsx
+++ trials/app/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
     const [num, setNum] = useState("")
     const [num1, setNum1] = useState([])
     const gg = async () => {
-        // const  datagg  = await axios.get("https://jsonplaceholder.typicode.com/users")
+        const  datagg  = await axios.get("https://jsonplaceholder.typicode.com/users")
         // setNum1({datagg})
         // return console.log(datagg)
     }
