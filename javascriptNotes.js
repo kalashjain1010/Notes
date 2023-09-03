8f3ba7b43c82af1d52d945f33cd306f32867f3bc
kalash <kalash@localhost.localdomain>
2023-09-04 02:05:48 +0530

    adding notes js

---
+++ javascriptNotes.js
@@ -0,0 +1,29 @@
+// word vs keyword     
+
+// var let const  -- inka diff kha use hote
+        // var - old js mai h  
+                // var - function scoped hota h => var apne parent fuction mai khi bhi  use ho skta h
+        // let const - new js mai
+                // let - change ho skta
+                // const - nhi ho skta change
+
+// hoisting  - value upr aajana
+// 
+// types - kese nikalye
+
+
+// primitive:number undifiend and refrence: array[] object{} ()
+
+
+// function 
+        // : 
+        //    function abc(){
+            // console
+        //    }
+        //    abc()
+// 
+    //   hello = () => {
+        //  return "Hello World!";
+        // }
+
+
