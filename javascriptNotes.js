48647aae2156760afcce1ca99b583b3c4a65d85d
kalash jain <kalashjain54@gmail.com>
2023-09-28 10:41:49 +0530

    yes

--- javascriptNotes.js
+++ javascriptNotes.js
@@ -49,3 +49,100 @@
 //         var a = 21
 //         return a
 // })()
+
+
+// console.log(a)
+// var a
+// a = 2
+
+// a= 2
+// b =a 
+// // console.log(b)
+// b=20
+// console.log(a)
+// console.log(b)
+
+// abc = [1,2,3,4];
+
+// // c= abc
+//  c= [...abc]
+//  c.pop()
+
+// console.log(c);
+// console.log(abc);
+// console.log(c)
+
+
+// function abc (x,y,z) {
+
+//   console.log("objectsfdadsfa")
+      
+// }
+
+
+// a = {
+//         a: "name",
+//         hello: function(){
+//                 console.log()
+//         }
+
+// }
+
+
+// function abc(){
+//         a=12 
+//         function cd(){
+//                 console.log("dasfasdf")
+//         }
+// }
+
+// if(-1){
+//         console.log("object")
+// }
+
+
+// a=[1,2,3,4,5]
+// console.log(typeof(a))
+// a= {
+        
+// }
+
+// function circle(color){
+//         this.radius = 12
+//         this.color = color 
+
+// }
+
+// var circle1 = new circle("red")
+// var circle2 = new circle("pink")
+
+// var human = {
+//         canWal: true,
+//         canSex: false,
+// }
+
+// var student ={
+//         canStudy: true
+// }
+
+// student__proto__ = human
+
+
+// var a =12
+// console.log(this)
+
+// (abc =  () =>{
+//         a=32
+//         console.log(this)
+// })()
+
+// abc()
+
+asdf = {
+        name: "jakaksdk",
+        hello: bc=()=>{
+                console.log(this)
+        }       
+}
+
+console.log(asdf)
